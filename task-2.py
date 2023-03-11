4133bad8eeffb9af66706909a7b10e264be3ddee
kgilmore3 <97620348+kgilmore3@users.noreply.github.com>
2023-03-11 07:03:08 -0700

    Update task-2.py

--- task-2.py
+++ task-2.py
@@ -27,6 +27,32 @@ for university in universities:
         # The university doesn't exist, so add it with a POST request
         response = requests.post(url, json=university)
         print(f"Added university: {university['name']}")
+        
+        
+        
+# import requests
+# import csv
+# # Set the API endpoint URL
+# url = 'http://universities.hipolabs.com/search?country=Canada'
+# # Send a GET request to the API endpoint and store the JSON response
+# response = requests.get(url)
+# data = response.json()
+# # Extract the necessary data from the JSON response
+# universities = []
+# for item in data:
+#     university = {}
+#     university['name'] = item['name']
+#     university['country'] = item['country']
+#     university['website'] = item['web_pages'][0] if len(item['web_pages']) > 0 else ''
+#     universities.append(university)
+
+# #Write the data to a CSV file
+# with open('canadian_universities.csv', 'w', newline='') as csvfile:
+#     fieldnames = ['name', 'country', 'website']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writeheader()
+#     for university in universities:
+#         writer.writerow(university)
 
 
 #using the requests library to send a GET request to the API endpoint and retrieve the JSON response
